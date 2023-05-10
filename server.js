d1f62b321f955c0c9fee2c1e7949da142dfa62cc
christopher brown <christopherbrown@christohersMini.fios-router.home>
2023-05-10 00:06:21 -0400

    added middleware and get requests

--- server.js
+++ server.js
@@ -3,5 +3,20 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+//middleware
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//get requests
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+app.get(`/api/notes`, (req, res) => {
+   res.json(notes);
+ });
